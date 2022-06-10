483f4793f344a0014dbc6c6f77820f014cf53332
maikl200 <syper.martunenko200@yandex.ru>
2022-06-10 19:02:23 +0300

    add mainPage

---
+++ my-app/src/components/MainPage/MainPage.tsx
@@ -0,0 +1,14 @@
+import React, {FC} from 'react';
+
+import style from './mainPage.module.scss'
+import NavBar from "../NavBar/NavBar";
+
+const MainPage: FC = () => {
+    return (
+        <main className={style.main}>
+            <NavBar/>
+        </main>
+    );
+};
+
+export default MainPage;
