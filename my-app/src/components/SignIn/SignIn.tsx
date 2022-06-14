e767fe0d3144f1aac45e7d04570b1ff8801fbc93
Fron <syper.martunenko200@yandex.ru>
2022-06-15 02:58:15 +0300

    started writing the validation for the login

--- my-app/src/components/SignIn/SignIn.tsx
+++ my-app/src/components/SignIn/SignIn.tsx
@@ -1,4 +1,4 @@
-import React, {FC} from 'react';
+import React, {FC, useState} from 'react';
 
 import style from './signIn.module.scss'
 import Input from "../../UI/Input/Input";
@@ -6,15 +6,42 @@ import ButtonUI from "../../UI/Button/ButtonUI";
 import imgReg from "../../assets/img_at_registration.png";
 import {useNavigate} from "react-router-dom";
 
+const initialTouched = {
+  valueEmail: false,
+  valuePassword: false,
+}
+
 const SignIn: FC = () => {
   const navigate = useNavigate()
+
+  const [valuePassword, setValuePassword] = useState<string>('')
+  const [valueEmail, setValueEmail] = useState<string>('')
+  const [errorPassword, setErrorPassword] = useState<string>('')
+  const [errorEmail, setErrorEmail] = useState<string>('')
+  const [touched, setTouched] = useState<object>(initialTouched)
   return (
     <main className={style.main}>
       <div className={style.main_logInBlock}>
         <form className={style.main_logInBlock_form}>
           <span className={style.main_logInBlock_title}>Sign in</span>
-          <Input title='Email' placeholder='Email' type='email' width='100%'/>
-          <Input title='Password' placeholder='Password' type='password' width='100%'/>
+          <Input defaultValue={valueEmail} onChange={(e) => {
+            setValueEmail(e.target.value)
+            setTouched({valueEmail: true})
+          }}
+                 title='Email'
+                 placeholder='Email'
+                 type='email'
+                 width='100%'/>
+          <Input
+            defaultValue={valuePassword}
+            onChange={(e) => {
+              setValuePassword(e.target.value)
+              setTouched({valuePassword: true})
+            }}
+            title='Password'
+            placeholder='Password'
+            type='password'
+            width='100%'/>
           <ButtonUI height='56px' title='Log in' padding='6px 12px' width='100%'/>
           <span onClick={() => navigate('/createAcc')} className={style.main_logInBlock_regPage}>Forgot password?</span>
         </form>
